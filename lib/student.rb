2ca118ae69f4e31b25f34ec9b26e550301e9319a
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-20 20:09:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id = nil)
+  def initialize(name, grade = nil, id = nil)
     @name = name
     @grade = grade
     @id = id
